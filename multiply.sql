25dea952264503d0424b5d03025dd00868383a21
Aibek <aitanaibek@mail.ru>
2025-08-21 06:58:10 +0500

    local sql run test with sqlite

--- multiply.sql
+++ multiply.sql
@@ -1,1 +1,5 @@
-SELECT price * amount AS total FROM items
+CREATE TABLE items (price INT, amount INT);
+
+INSERT INTO items VALUES (10, 5), (20, 2);
+
+SELECT price, amount, price * amount AS total FROM items;
