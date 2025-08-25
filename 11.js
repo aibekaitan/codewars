d62e648999c942aea6fdc06e121b65d7d62d7624
Aibek <aitanaibek@mail.ru>
2025-08-26 04:21:52 +0500

    1

---
+++ 11.js
@@ -0,0 +1,11 @@
+function removeDuplicateWords(s) {
+  const seen = new Set();
+  return s
+    .split(' ')
+    .filter(word => {
+      if (seen.has(word)) return false;
+      seen.add(word);
+      return true;
+    })
+    .join(' ');
+}
