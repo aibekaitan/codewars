e17d695a2b0b3b2da0d7f7d572646e2ac5831496
Aibek <aitanaibek@mail.ru>
2025-08-26 04:22:05 +0500

    1

---
+++ 12.js
@@ -0,0 +1,8 @@
+function factorial(n) {
+  if (n < 0) throw new RangeError("n must be non-negative");
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
