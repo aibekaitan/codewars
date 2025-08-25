45e3fa2b0c8228727b906b349d6c2f17fd5deb31
Aibek <aitanaibek@mail.ru>
2025-08-26 04:18:38 +0500

    1

---
+++ 7.sql
@@ -0,0 +1,12 @@
+SELECT
+  p.pokemon_name,
+  (p.str * m.multiplier) AS modifiedStrength,
+  m.element
+FROM
+  pokemon AS p
+JOIN
+  multipliers AS m ON p.element_id = m.id
+WHERE
+  (p.str * m.multiplier) >= 40
+ORDER BY
+  modifiedStrength DESC;
